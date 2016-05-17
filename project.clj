1cedfe53464976bbda6f8b331be58d29b99a903c
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-05-17 16:05:44 +0200

    project: prepare for next release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spootnik/reporter "0.1.9"
+(defproject spootnik/reporter "0.1.10-SNAPSHOT"
   :description "error and event reporting component"
   :url "https://github.com/pyr/reporter"
   :license {:name "MIT/ISC"}
