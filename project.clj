315d25ef628906c5e9635d8174e4bbb068535fb6
Pierre-Yves Ritschard <pyr@spootnik.org>
2017-12-11 17:39:41 +0100

    project: release 0.1.18

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject spootnik/reporter "0.1.18-SNAPSHOT"
+(defproject spootnik/reporter "0.1.18"
   :description "error and event reporting component"
   :url "https://github.com/pyr/reporter"
   :license {:name "MIT/ISC"}
   :profiles {:dev {:global-vars {*warn-on-reflection* true}}}
-  :dependencies [[org.clojure/clojure        "1.9.0-alpha14"]
-                 [org.clojure/tools.logging  "0.3.1"]
+  :dependencies [[org.clojure/clojure        "1.9.0"]
+                 [org.clojure/tools.logging  "0.4.0"]
                  [com.stuartsierra/component "0.3.2"]
-                 [spootnik/raven             "0.1.2"]
-                 [spootnik/net               "0.3.3-beta9"]
+                 [spootnik/raven             "0.1.3"]
+                 [spootnik/net               "0.3.3-beta13"]
                  [spootnik/uncaught          "0.5.3"]
                  [prismatic/schema           "1.1.3"]
                  [metrics-clojure            "2.9.0"]
