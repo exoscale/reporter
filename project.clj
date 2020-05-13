2299fc73c4161f892d27764546b642a300c1a859
Pierre-Yves Ritschard <pyr@spootnik.org>
2020-05-13 08:51:27 +0200

    Version 0.1.49

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/reporter "0.1.49-SNAPSHOT"
+(defproject exoscale/reporter "0.1.49"
   :description "error and event reporting component"
   :url "https://github.com/exoscale/reporter"
   :license {:name "MIT/ISC"}
