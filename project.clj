18f0ed032e19ae61ed361293d204b802f051e8d9
Max Penet <max.penet@exoscale.ch>
2020-05-19 12:34:21 +0200

    Version 0.1.51

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/reporter "0.1.51-SNAPSHOT"
+(defproject exoscale/reporter "0.1.51"
   :description "error and event reporting component"
   :url "https://github.com/exoscale/reporter"
   :license {:name "MIT/ISC"}
