f4e477fd7cdccb76ea7743e09856383abe5a8105
Miguel Ping <miguel.ping@exoscale.ch>
2020-05-28 16:35:29 +0100

    Separate spec kw per domain

---
+++ src/spootnik/reporter/specs.clj
@@ -0,0 +1,72 @@
+(ns spootnik.reporter.specs
+  (:require [clojure.spec.alpha :as s]))
+
+
+;; SSL
+
+(s/def :spootnik.reporter.config/bundle string?)
+(s/def :spootnik.reporter.config/password string?)
+(s/def :spootnik.reporter.config/ssl-bundle 
+  (s/keys :req-un [:spootnik.reporter.config/bundle 
+                   :spootnik.reporter.config/password]))
+(s/def :spootnik.reporter.config/cert string?)
+(s/def :spootnik.reporter.config/ca-cert string?)
+(s/def :spootnik.reporter.config/authority string?)
+(s/def :spootnik.reporter.config/pkey string?)
+(s/def :spootnik.reporter.config/ssl-cert (s/keys :req-un [:spootnik.reporter.config/cert :spootnik.reporter.config/authority :spootnik.reporter.config/pkey]))
+(s/def :spootnik.reporter.config/ssl (s/or :bundle :spootnik.reporter.config/ssl-bundle :cert :spootnik.reporter.config/ssl-cert))
+
+;; Generic
+(s/def :spootnik.reporter.config/port pos-int?)
+(s/def :spootnik.reporter.config/host string?)
+(s/def :spootnik.reporter.config/protocol string?)
+(s/def :spootnik.reporter.config/tls :spootnik.reporter.config/ssl-cert)
+(s/def :spootnik.reporter.config/endpoint (s/and string? #(clojure.string/starts-with? % "/")))
+
+;; riemann
+(s/def :spootnik.reporter.config.riemann/batch pos-int?)
+(s/def :spootnik.reporter.config.riemann/defaults any?)
+
+(s/def :spootnik.reporter.config/riemann
+  (s/keys :req-un [:spootnik.reporter.config/host]
+          :opt-un [:spootnik.reporter.config/port
+                   :spootnik.reporter.config/protocol
+                   :spootnik.reporter.config.riemann/batch
+                   :spootnik.reporter.config.riemann/defaults
+                   :spootnik.reporter.config/tls]))
+
+;; single metrics reporter
+(s/def :spootnik.reporter.config.metrics.reporter.config/opts map?)
+(s/def :spootnik.reporter.config.metrics.reporter.config/interval pos-int?)
+(s/def :spootnik.reporter.config.metrics.reporter/config
+  (s/keys :req-un []
+          :opt-un [:spootnik.reporter.config.metrics.reporter.config/interval
+                   :spootnik.reporter.config.metrics.reporter.config/opts]))
+
+;; metrics
+(s/def :spootnik.reporter.config.metrics/reporters
+  (s/map-of #{:graphite :prometheus :riemann :console :jmx}
+            :spootnik.reporter.config.metrics.reporter/config))
+(s/def :spootnik.reporter.config/metrics
+  (s/keys :req-un [:spootnik.reporter.config.metrics/reporters]))
+
+;; sentry
+(s/def :spootnik.reporter.config.sentry/dsn string?)
+(s/def :spootnik.reporter.config/sentry
+  (s/keys :req-un [:spootnik.reporter.config.sentry/dsn]))
+
+
+;; prometheus
+(s/def :spootnik.reporter.config/prometheus (s/keys :req-un [:spootnik.reporter.config/port]
+                                                    :opt-un [:spootnik.reporter.config/tls
+                                                             :spootnik.reporter.config/endpoint
+                                                             :spootnik.reporter.config/host]))
+;; General config
+(s/def :spootnik.reporter.config/prevent-capture? boolean?)
+(s/def :spootnik.reporter/config
+  (s/keys :req-un []
+          :opt-un [:spootnik.reporter.config/prevent-capture?
+                   :spootnik.reporter.config/sentry
+                   :spootnik.reporter.config/metrics
+                   :spootnik.reporter.config/riemann
+                   :spootnik.reporter.config/prometheus]))
---
+++ test/spootnik/reporter/specs_test.clj
@@ -0,0 +1,41 @@
+(ns spootnik.reporter.specs-test
+  (:require [clojure.test :refer :all]
+            [clojure.spec.alpha :as s]
+            [spootnik.reporter.specs]))
+
+(def valid-reporter-config
+  {:sentry {:dsn "https://a02ce81f94ed426588508ffc4f05d999:38f389ed20794158ba1a649c61cd1897@errors.exoscale.ch/104"}
+   :prometheus {:port 8007}
+   :riemann {:host     "infra-mon-pp001.gv2.p.exoscale.net"
+             :port     5554
+             :protocol "tls"
+
+             :defaults {:ttl  600
+                        :host "bundes-volumes-preprod.gva2"
+                        :tags ["bundes-volumes" "graph"]}
+
+             :tls {:cert      "/etc/host-certificate/ssl/cert.pem"
+                   :authority "/etc/host-certificate/ssl/ca.pem"
+                   :pkey      "/etc/host-certificate/ssl/key.pkcs8"}}
+   :metrics {:reporters {:riemann {:interval 10
+                                   :opts     {:ttl       20
+                                              :tags      ["bundes-volumes" "graph"]
+                                              :host-name "bundes-volumes-preprod.gva2"}}}}})
+
+(deftest reporter-spec-validates-correctly-test
+  (testing "A correct spec should be valid"
+    (is (empty? (s/explain-data :spootnik.reporter/config valid-reporter-config))))
+
+  ;; Test a couple of locations, dont need to be exhaustive
+  (testing "Prometheus port should be a positive int"
+    (is (some? (s/explain-data :spootnik.reporter/config (assoc-in valid-reporter-config [:prometheus :port] -1)))))
+
+  (testing "Prometheus host should not be empty"
+    (is (some? (s/explain-data :spootnik.reporter/config (assoc-in valid-reporter-config [:prometheus :host] nil)))))
+
+  (testing "Riemann host should not be empty"
+    (is (some? (s/explain-data :spootnik.reporter/config (assoc-in valid-reporter-config [:riemann :host] nil)))))
+
+  (testing "Metrics Reporters set is fixed"
+    (is (some? (s/explain-data :spootnik.reporter/config (assoc-in valid-reporter-config [:metrics :reporters] {:foundationdb {}}))))))
+
