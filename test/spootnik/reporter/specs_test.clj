0caa4c10024f354048b622613cd06811779530aa
Miguel Ping <59603303+mping-exo@users.noreply.github.com>
2020-06-09 14:08:18 +0100

    Update test/spootnik/reporter/specs_test.clj
    Co-authored-by: Aaron Groom <32138718+aarongroom@users.noreply.github.com>

--- test/spootnik/reporter/specs_test.clj
+++ test/spootnik/reporter/specs_test.clj
@@ -4,7 +4,7 @@
             [spootnik.reporter.specs]))
 
 (def valid-reporter-config
-  {:sentry {:dsn "https://dummy:dsn@errors.exoscale.ch/31337"}
+  {:sentry {:dsn "https://dummy:dsn@errors.sentry-host.com/31337"}
    :prometheus {:port 8007}
    :riemann {:host     "infra-mon-pp001.gv2.p.exoscale.net"
              :port     5554
@@ -38,4 +38,3 @@
 
   (testing "Metrics Reporters set is fixed"
     (is (some? (s/explain-data :spootnik.reporter/config (assoc-in valid-reporter-config [:metrics :reporters] {:foundationdb {}}))))))
-
