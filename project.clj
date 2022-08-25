6130a7583a36549be66c8a24bced2ffb65ba1cc2
Wouter Dullaert <wouter.dullaert@exoscale.ch>
2022-08-25 10:52:11 +0200

    Version 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/reporter "0.1.64"
+(defproject exoscale/reporter "0.2.0-SNAPSHOT"
   :description "error and event reporting component"
   :url "https://github.com/exoscale/reporter"
   :license {:name "MIT/ISC"}
