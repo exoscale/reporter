f11a32f076283332ba00c7413ddc1514f76afaab
Albin Parou <albin.parou@exoscale.ch>
2024-02-26 10:53:10 +0100

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/reporter "0.2.0-SNAPSHOT"
+(defproject exoscale/reporter "0.2.0"
   :description "error and event reporting component"
   :url "https://github.com/exoscale/reporter"
   :license {:name "MIT/ISC"}
