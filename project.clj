ec165496717232bfb471a0b2fe0a5a20d9b5c81b
Miguel Ping <miguel.ping@exoscale.ch>
2024-03-20 16:22:27 +0000

    Version 1.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/reporter "1.0.1"
+(defproject exoscale/reporter "1.0.2-SNAPSHOT"
   :description "error and event reporting component"
   :url "https://github.com/exoscale/reporter"
   :license {:name "MIT/ISC"}
